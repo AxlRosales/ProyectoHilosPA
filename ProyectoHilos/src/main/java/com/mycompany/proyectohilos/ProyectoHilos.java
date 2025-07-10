3f4c1f0a03e42b91bbd662783b0d60aceafe02c8
AxlRosales <axelerl2000@gmail.com>
2025-07-10 17:51:03 -0600

    first commit

---
+++ ProyectoHilos/src/main/java/com/mycompany/proyectohilos/ProyectoHilos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectohilos;
+
+/**
+ *
+ * @author Axel Rosales
+ */
+public class ProyectoHilos {
+
+    public static void main(String[] args) {
+        Reloj form = new Reloj();
+        form.show();
+    }
+}
